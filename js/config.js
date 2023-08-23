d2df989f3375e01f804233350c9853f5b6f8ab6e
Matheus-OAMK <t2fema00@students.oamk.fi>
2023-08-23 14:39:01 +0300

    config settings

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-central-1_Cd1Ey7LcZ', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5pr2a1v6mtj5m0lj89s8qta03u', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-central-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
